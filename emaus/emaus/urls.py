206bce2719268c51616ae306791b5940b47fddbb
jfcosm <francisco.carle@live.com>
2025-06-26 15:34:15 -0400

    Primera versión funcional local de Emaús

--- emaus/emaus/urls.py
+++ emaus/emaus/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('documentos/', include('documentos.urls')),
     path('sacramentos/', include('sacramentos.urls')),
